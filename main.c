8d9093f3be186b8f2fa921608ebe9eef67784ecc
miguel-dev <miguelacortes@zoho.com>
2019-05-15 02:00:03 +0000

    [Feature] More basic functionality

--- main.c
+++ main.c
@@ -11,8 +11,9 @@ int main(int argc, char *argv[])
 	if(argc > 1)
 	{
 		FILE *file;
-		/*char *opcode_token;*/
+		char *token;
 		char *buffer;
+		char *delimiters = " \t\r\n\v\f";
 		size_t buffsize = 32;
 		ssize_t line_size;
 		file = fopen(argv[1], "r");
@@ -29,13 +30,19 @@ int main(int argc, char *argv[])
 		}
 		line_size = getline(&buffer, &buffsize, file);
 
-/*		if (buffer == NULL)
-		{
-			printf("line_size error");
-			}*/
 		while (line_size >= 0)
 		{
 			printf("%s", buffer);
+			token = strtok(buffer, delimiters);
+			if (token == NULL)
+			{
+				free(buffer);
+				continue;
+			}
+			while (token != NULL)
+			{
+				token = strtok(buffer, delimiters);
+			}
 			line_size = getline(&buffer, &buffsize, file);
 		}
 		free(buffer);
