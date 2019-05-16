d8ca692f7bd1a62e08e00403aed8a7054f45337a
javb92 <mentor37_@hotmail.com>
2019-05-16 01:12:08 +0000

    functional code

--- functions.c
+++ functions.c
@@ -10,29 +10,48 @@
  */
 void push(stack_t **head, unsigned int n)
 {
-	stack_t *new_tail = malloc(sizeof(stack_t));
-	stack_t *fast_iter = *head;
+	stack_t *new_head = malloc(sizeof(stack_t));
 
-	printf("FUNCTION PUSH");
-	if (new_tail == NULL)
+	if (new_head == NULL)
 	{
-		free(new_tail);
+		free(new_head);
 		return;
 	}
-	new_tail->n = n;
-	new_tail->next = NULL;
-	if (*head == NULL)
+	new_head->n = n;
+	new_head->next = *head;
+	new_head->prev = NULL;
+	if (*head != NULL)
 	{
-		new_tail->prev = NULL;
-		*head = new_tail;
-		return;
+		(*head)->prev = new_head;
 	}
-	while (fast_iter->next != NULL)
-	{
-		fast_iter = fast_iter->next;
-	}
-	fast_iter->next = new_tail;
-	new_tail->prev = fast_iter;
+	*head = new_head;
+	return;
+
+}
+void _posh(stack_t **head, unsigned int n)
+{
+        stack_t *new_tail = malloc(sizeof(stack_t));
+        stack_t *fast_iter = *head;
+
+        if (new_tail == NULL)
+        {
+                free(new_tail);
+                return;
+        }
+        new_tail->n = n;
+        new_tail->next = NULL;
+        if (*head == NULL)
+        {
+                new_tail->prev = NULL;
+                *head = new_tail;
+                return;
+        }
+        while (fast_iter->next != NULL)
+        {
+                fast_iter = fast_iter->next;
+        }
+        fast_iter->next = new_tail;
+        new_tail->prev = fast_iter;
 
 }
 /**
@@ -47,7 +66,6 @@ void pall(stack_t **head, unsigned int n)
 	stack_t *tmp = *head;
 	(void)(n);
 
-	printf("FUNCTION PALL");
 	if (tmp == NULL)
 	{
 		return;
--- main.c
+++ main.c
@@ -19,12 +19,15 @@ int main(int argc, char *argv[])
 	FILE *file;
 	char *token;
 	char *buffer;
-	char *delimiters = " \t\r\n\v\f";
+	char *delimiters = " \t\n";
 	size_t buffsize = 32;
 	ssize_t line_size;
 	int f_idx = 0;
 	file = fopen(argv[1], "r");
-
+	/*(void)(head);
+	(void)(f_idx);
+	(void)(par_number);
+	(void)(ints);*/
 	if(argc > 1)
 	{
 		head = NULL;
@@ -51,23 +54,20 @@ int main(int argc, char *argv[])
 			}
 			while (token != NULL)
 			{
-				for(f_idx = 0 ; ints[f_idx].opcode != NULL ; f_idx++)
+				while(ints[f_idx].opcode != NULL)
                                 {
-					/*printf(" |opcode: (%s)| ", ints[f_idx].opcode);*/
-                                        if(strcmp(ints[f_idx].opcode, token) == 0)
+					if(strcmp(ints[f_idx].opcode, token) == 0)
                                         {
 						if(strcmp(token, "push") == 0)
+						{
 							par_number = atoi(strtok(NULL, delimiters));
-                                                /*printf("found: (%s) in: (%d)|", ints[f_idx].opcode, par_number);*/
+						}
 						ints[f_idx].f(&head, par_number);
                                         }
-					/*else if(strcmp("pil", token) == 0)
-					{
-						ints[f_idx].f(&head, par_number);
-						}*/
-					/*printf(" |opcode: (%s)| ", ints[f_idx].opcode);*/
+					f_idx++;
 				}
 				token = strtok(NULL, delimiters);
+				f_idx = 0;
 			}
 			line_size = getline(&buffer, &buffsize, file);
 		}
